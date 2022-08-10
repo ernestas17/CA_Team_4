46d28d6646c5dfd371345c1c61a1444941ffb942
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-10 17:25:58 +0300

    array of navigation links  objects created

---
+++ src/constants/navLinks.js
@@ -0,0 +1,9 @@
+const NAV_LINKS = [
+  { link: '/', name: 'Home' },
+  { link: '/about', name: 'About us' },
+  { link: '/services', name: 'Services' },
+  { link: '/blog', name: 'Blog' },
+  { link: '/contact', name: 'Contact us' },
+];
+
+export default NAV_LINKS;
