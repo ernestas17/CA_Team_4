8f59cd4acc0a71a55e1702e9a2cc0d30739f760f
gintaren <104127383+gintaren@users.noreply.github.com>
2022-08-10 21:34:27 +0300

    Update Button.style.jsx

--- src/components/Button/Button.style.jsx
+++ src/components/Button/Button.style.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const StyledButtton = styled.button`
+export const StyledButton = styled.button`
   background-color: ${(props) => (props.primary ? '#FF6433' : '#1E1B1B')};
   color: #ffffff;
   padding: 16px 40px;
@@ -14,8 +14,5 @@ export const StyledButtton = styled.button`
 
   display: flex;
   flex-direction: column;
-
-  &:hover {
-    cursor: pointer;
-  }
+  cursor: pointer;
 `;
