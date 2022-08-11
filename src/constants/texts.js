305790e014e39b24ed0d9d2526d8e1cbccfb61f4
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-11 20:44:15 +0300

    texts added

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -1,12 +1,22 @@
 const TEXTS = {
+  homePage: {
+    sectionOne: {
+      heading: 'Get your vehicle sevice done online at one place',
+      button: 'Book a service',
+      title: 'We are open',
+      workingHours: 'Monday to Friday 9:00 AM to 10:00 AM',
+      hours: '24',
+      hoursSh: 'hr',
+      service: 'Quick service',
+      rating: 'Rating',
+    },
+    sectionTWo: {},
+  },
   callToAction: {
     heading: 'Get in touch with us for your service related query',
     button: 'Contact Us',
   },
   footerContacts: {
-    titles: {
-      number: '',
-    },
     heading: 'Get in touch with us for your service',
     phoneNr: '1800 2665 24 52',
     address: 'NH 234 Public Square San Francisco 65368',
