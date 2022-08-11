0334d8d62b68c128143a8b91439e7a21394449a7
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-11 16:03:55 +0300

    texts object file added

---
+++ src/constants/texts.js
@@ -0,0 +1,20 @@
+const TEXTS = {
+  callToAction: {
+    heading: 'Get in touch with us for your service related query',
+    button: 'Contact Us',
+  },
+  footerContacts: {
+    titles: {
+      number: '',
+    },
+    heading: 'Get in touch with us for your service',
+    phoneNr: '1800 2665 24 52',
+    address: 'NH 234 Public Square San Francisco 65368',
+    workingHours: 'Monday to Friday 9:00 AM to 10:00 AM',
+  },
+  footer: {
+    name: 'Finsweet',
+  },
+};
+
+export default TEXTS;
