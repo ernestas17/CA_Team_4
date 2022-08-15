a1f249b05eea75721d5092b61af7b40617f1e946
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-15 21:55:24 +0300

    imports added

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,10 @@
 import CallToAction from '../../components/CallToAction';
 import HeroSection from './components/HeroSection';
 import ServicesSection from './components/ServicesSection/ServicesSection';
+import ProcessSection from './components/processSection/ProcessSection';
+import ExperienceSection from './components/ExperienceSection/ExperienceSection';
+import BrandsSection from './components/BrandsSection';
+import ReviewsSection from './components/ReviewsSecton';
 
 import { StyledHomePage } from './HomePage.style';
 
@@ -9,7 +13,11 @@ const HomePage = () => {
     <StyledHomePage>
       <HeroSection />
       <ServicesSection />
+      <ProcessSection />
       <CallToAction />
+      <ExperienceSection />
+      <BrandsSection />
+      <ReviewsSection />
     </StyledHomePage>
   );
 };
