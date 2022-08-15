38caf7765206c2d697b70993323c28e0e214cf22
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-15 21:55:04 +0300

    texts added

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -13,8 +13,90 @@ const TEXTS = {
         text: 'Rating',
       },
     },
-    sectionTwo: {
-      heading1: 'We are taking car servicing seriously',
+    servicesSection: {
+      services: {
+        heading: 'We are taking car servicing seriously',
+        service1: {
+          title: 'Convenient service',
+          text: 'Through True Ritch Attended does no end it his mother since real had half every him.',
+        },
+        service2: {
+          title: 'Expert mechanics',
+          text: 'Through True Ritch Attended does no end it his mother since real had half every him.',
+        },
+        service3: {
+          title: 'Transparent pricing',
+          text: 'Through True Ritch Attended does no end it his mother since real had half every him.',
+        },
+        button: {
+          text: 'Know more about us',
+          link: '#',
+        },
+      },
+      form: {
+        title: 'Get a quote for the car service',
+        paceholder: 'Enter your location',
+        button: {
+          text: 'Get your quote',
+          link: '#',
+        },
+      },
+    },
+    processSection: {
+      heading: 'We follow a clear process to help you out.',
+      paragraph:
+        'Through True Rich Attended does no end it his mother since real had half every him case in packages enquire',
+      buttonTxt: 'Learn more',
+      step1: {
+        number: '01',
+        title: 'Get a Quote',
+        text: 'Through True Rich Attended does no end it his mother since real had half every.',
+      },
+      step2: {
+        number: '02',
+        title: 'Book an Appointment',
+        text: 'Through True Rich Attended does no end it his mother since real had half every.',
+      },
+      step3: {
+        number: '03',
+        title: 'Get your Service Done',
+        text: 'Through True Rich Attended does no end it his mother since real had half every.',
+      },
+      cardsSection: {},
+    },
+    experienceSection: {
+      experience1: {
+        number: '20+',
+        text: 'Proffesional',
+      },
+      experience2: {
+        number: '10+',
+        text: 'Years Eperience',
+      },
+      experience3: {
+        number: '12K+',
+        text: 'Service Closed',
+      },
+      experience4: {
+        number: '100%',
+        text: 'Customer Satisfaction',
+      },
+    },
+    brandsSection: {
+      title: 'Brands we Serve',
+    },
+    reviwsSection: {
+      heading: 'Our customers say the nicest things about our service',
+      card1: {
+        name: 'Jonathan Vallem',
+        city: 'New york',
+        text: 'I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car in a short comprehensive look on all the working',
+      },
+      card2: {
+        name: 'Smith Johnson',
+        city: 'New york',
+        text: 'I have got my car repaired at Finsweet many times before, they are good at what they do. Not only did they repair my car in a short comprehensive look on all the working',
+      },
     },
   },
   callToAction: {
