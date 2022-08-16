304e688a56260c6fc4c15dffcc23a6c2ca4a6669
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-16 17:39:17 +0300

    reviews section created

--- src/pages/HomePage/components/ReviewsSecton/ReviewsSection.jsx
+++ src/pages/HomePage/components/ReviewsSecton/ReviewsSection.jsx
@@ -1,10 +1,24 @@
 import TEXTS from '../../../../constants/texts';
-import { StyledRewiewSection } from './ReviewsSection.style';
+import user1 from '../../../../assets/images/homePage/reviewsSection/user1.jpg';
+import user2 from '../../../../assets/images/homePage/reviewsSection/user2.jpg';
+import {
+  StyledRewiewSection,
+  StyledReviewsContainer,
+  StyledCard,
+  StyledUserBox,
+} from './ReviewsSection.style';
 
 const ReviewsSection = () => {
   return (
     <StyledRewiewSection>
       <h3>{TEXTS.homePage.reviwsSection.heading}</h3>
+      <StyledReviewsContainer>
+        <StyledCard>
+          <StyledUserBox>
+            <img src={user1} alt='user' />
+          </StyledUserBox>
+        </StyledCard>
+      </StyledReviewsContainer>
     </StyledRewiewSection>
   );
 };
