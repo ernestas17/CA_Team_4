67ca39d1d5d38ded6c284e27bf456ae348b8032a
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-18 21:36:11 +0300

    router implemented

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import NAV_LINKS from './constants/navLinks';
 // Assets imports
 import footerLogo from './assets/icons/footer/Logo.svg';
 // Component imports
-import FooterContacts from './components/FooterContacts';
 import Footer from './components/Footer';
+import Header from './components/Header';
 // Page imports
 import HomePage from './pages/HomePage';
+import AboutUsPage from './pages/AboutUsPage';
+import ServicesPage from './pages/ServicesPage';
+import BlogPage from './pages/BlogPage';
+import ContactPage from './pages/ContactPage';
+// Links
+import links from './constants/navLinks';
+// Router
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <HomePage />
-      <FooterContacts />
+      <Header navLinks={links} />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutUsPage />} />
+        <Route path='/services' element={<ServicesPage />} />
+        <Route path='/blog' element={<BlogPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+      </Routes>
       <Footer logo={footerLogo} navLinks={NAV_LINKS} />
     </>
   );
