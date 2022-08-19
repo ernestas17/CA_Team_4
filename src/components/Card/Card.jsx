90b6e10749f9827af262c0c3a7f37fdd60466967
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-19 17:02:43 +0300

    card component finished and fixed

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+import { StyledCard, StyledCardText } from './Card.style';
+
+const Card = ({ icon, text }, props) => {
+  return (
+    <StyledCard
+      icon={icon}
+      customWidth={props.customWidth}
+      customHeight={props.customHeight}>
+      <img src={icon} alt='icon' />
+      <StyledCardText>{text}</StyledCardText>
+    </StyledCard>
+  );
+};
+
+export default Card;
