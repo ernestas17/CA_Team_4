e8f4de47cfdd64dc7f97f495492ae42b56335fea
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-21 17:12:46 +0300

    lazy loading implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
+import { lazy } from 'react';
 // Constants imports
 import NAV_LINKS from './constants/navLinks';
 // Assets imports
 import footerLogo from './assets/icons/footer/Logo.svg';
-// Component imports
-import Footer from './components/Footer';
-import Header from './components/Header';
-// Page imports
-import HomePage from './pages/HomePage';
-import AboutUsPage from './pages/AboutUsPage';
-import ServicesPage from './pages/ServicesPage';
-import BlogPage from './pages/BlogPage';
-import ContactPage from './pages/ContactPage';
 // Links
 import links from './constants/navLinks';
 // Router
 import { Routes, Route } from 'react-router-dom';
+// Page imports
+const HomePage = lazy(() => import('./pages/HomePage'));
+const AboutUsPage = lazy(() => import('./pages/AboutUsPage'));
+const ServicesPage = lazy(() => import('./pages/ServicesPage'));
+const BlogPage = lazy(() => import('./pages/BlogPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+// Component imports
+const Header = lazy(() => import('./components/Header'));
+const Footer = lazy(() => import('./components/Footer'));
 
 function App() {
   return (
