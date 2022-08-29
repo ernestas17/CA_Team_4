f1555b4d7e0c125acf203a51354ebc4f3e78e24e
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-29 11:43:24 +0300

    Responsiveness adjusted, themes  &brekpoints added

--- src/pages/HomePage/components/CardsSection/CardsSection.style.jsx
+++ src/pages/HomePage/components/CardsSection/CardsSection.style.jsx
@@ -5,10 +5,13 @@ export const StyledCardsSection = styled.section`
   background: ${({ theme }) => theme.color.lightGrey};
   width: 100%;
   padding: 64px 20px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media screen and (min-width: ${BREAKPOINTS.md}) {
+    padding: 64px 40px;
+  }
 
   @media screen and (min-width: ${BREAKPOINTS.lg}) {
     padding: 128px 40px;
@@ -31,9 +34,11 @@ export const StyledCardsSection = styled.section`
 `;
 
 export const StyledCardsContainer = styled.div`
+  width: 100%;
   display: grid;
+  justify-content: center;
   gap: 8px;
-  grid-template-columns: 1fr;
+  grid-template-columns: 1fr 1fr;
   margin-bottom: 32px;
 
   @media screen and (min-width: ${BREAKPOINTS.md}) {
@@ -41,28 +46,16 @@ export const StyledCardsContainer = styled.div`
   }
 
   @media screen and (min-width: ${BREAKPOINTS.xl}) {
-    grid-template-columns: 1fr 1fr 1fr;
   }
 
   @media screen and (min-width: ${BREAKPOINTS.xxl}) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 316px 316px 316px 316px;
   }
 `;
 
 export const StyledLinkWrapper = styled.div`
-  width: 316px;
+  width: 100%;
+  max-width: 1288px;
   display: flex;
   justify-content: flex-end;
-
-  @media screen and (min-width: ${BREAKPOINTS.lg}) {
-    width: 638px;
-  }
-
-  @media screen and (min-width: ${BREAKPOINTS.xl}) {
-    width: 960px;
-  }
-
-  @media screen and (min-width: ${BREAKPOINTS.xxl}) {
-    width: 1282px;
-  }
 `;
