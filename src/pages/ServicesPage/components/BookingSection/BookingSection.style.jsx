10e4938891dde0c5191ef8d1979ca35e828edf5f
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-08-30 23:07:05 +0300

    Themes and BP added

--- src/pages/ServicesPage/components/BookingSection/BookingSection.style.jsx
+++ src/pages/ServicesPage/components/BookingSection/BookingSection.style.jsx
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
+import BREAKPOINTS from '../../../../constants/breakpoints';
 
 export const StyledBookingSection = styled.section`
-  padding: 80px 0px 128px 0px;
+  padding: 64px 0px 64px 0px;
+
+  @media screen and (min-width: ${BREAKPOINTS.lg}) {
+    padding: 80px 0px 128px 0px;
+  }
 `;
--- src/pages/ServicesPage/components/ProcessSection/ProcessSection.style.jsx
+++ src/pages/ServicesPage/components/ProcessSection/ProcessSection.style.jsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import BREAKPOINTS from '../../../../constants/breakpoints.js';
 
 export const StyledProcessSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 128px 10px;
+  padding: 64px 20px;
   max-width: 1300px;
   margin: 0 auto;
   gap: 64px;
 
-  @media screen and (min-width: 1400px) {
+  @media screen and (min-width: ${BREAKPOINTS.lg}) {
     padding: 128px 0;
   }
 `;
@@ -19,7 +20,7 @@ export const StyledTopPart = styled.div`
   flex-direction: column;
   gap: 64px;
 
-  @media screen and (min-width: 1400px) {
+  @media screen and (min-width: ${BREAKPOINTS.xxl}) {
     flex-direction: row;
     justify-content: center;
 
@@ -27,101 +28,15 @@ export const StyledTopPart = styled.div`
   }
 `;
 
-export const StyledCardTop = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  & h3 {
-    font-weight: 700;
-    font-size: 48px;
-    line-height: 140%;
-    letter-spacing: -0.03em;
-    color: #1e1b1b;
-    max-width: 489px;
-  }
-
-  & ul {
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
-
-    margin-top: 48px;
-
-    & li {
-      display: flex;
-      gap: 24px;
-      align-items: baseline;
-      list-style: none;
-      width: 100%;
-
-      & p {
-        width: 80%;
-        max-width: 443px;
-        font-weight: 600;
-        font-size: 24px;
-        line-height: 36px;
-        letter-spacing: -0.03em;
-        color: #1e1b1b;
-
-        @media screen and (min-width: 820px) {
-          width: 100%;
-        }
-      }
-    }
-  }
-`;
-
-export const StyledCircle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 48px;
-  width: 48px;
-  border-radius: 50%;
-  background-color: #00d289;
-`;
-
 export const StyledBottomPart = styled.div`
   display: flex;
   flex-direction: column;
   gap: 64px;
 
-  @media screen and (min-width: 1400px) {
+  @media screen and (min-width: ${BREAKPOINTS.xxl}) {
     flex-direction: row;
     justify-content: center;
 
     gap: 88px;
   }
-`;
-
-export const StyledCardBottom = styled.div`
-  display: flex;
-  flex-direction: column;
-  max-width: 547px;
-
-  @media screen and (min-width: 820px) {
-    order: -1;
-  }
-
-  @media screen and (min-width: 1400px) {
-    order: 0;
-  }
-
-  & h3 {
-    font-weight: 700;
-    font-size: 48px;
-    line-height: 140%;
-    letter-spacing: -0.03em;
-    color: #1e1b1b;
-  }
-`;
-
-export const StyledCardText = styled.p`
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 175%;
-  color: #232536;
-  opacity: 0.6;
-  margin: 32px 0;
-  max-width: 512px;
 `;
