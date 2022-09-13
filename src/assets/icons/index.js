a082fbf1b3a2958635de7e60c4b1f890caa23874
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-09-13 10:13:37 +0300

    Icon deleted

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -155,27 +155,6 @@ export const ArrowIcon = (props) => (
   </svg>
 );
 
-export const LineIcon = (props) => (
-  <svg
-    width='2'
-    height='71'
-    viewBox='0 0 2 71'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-    {...props}>
-    <line
-      x1='1'
-      y1='1'
-      x2='0.999997'
-      y2='70'
-      stroke='currentColor'
-      strokeWidth='2'
-      strokeLinecap='round'
-      strokeDasharray='4 4'
-    />
-  </svg>
-);
-
 export const DiagnosticIcon = (props) => {
   return (
     <svg
