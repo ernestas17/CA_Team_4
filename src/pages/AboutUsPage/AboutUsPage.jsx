525d0d6626fd8bace3ea94b0d0bd50c82abbfa47
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-09-14 16:01:49 +0300

    Sections added to page

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,11 +1,15 @@
 import { StyledAboutUsPage } from './AboutUsPage.style';
 import BookingSection from './components/BookingSection/BookingSection';
 import ProcessSection from './components/ProcessSection/ProcessSection';
+import ObjectivesSection from './components/ObjectivesSection';
+import ServicesSection from './components/ServicesSection';
 const AboutUsPage = () => {
   return (
     <StyledAboutUsPage>
       <BookingSection />
       <ProcessSection />
+      <ObjectivesSection />
+      <ServicesSection />
     </StyledAboutUsPage>
   );
 };
