b051d309da325ae487e04a64a9cca20ce769358d
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-09-15 15:40:06 +0300

    Component renamed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const AboutUsPage = lazy(() => import('./pages/AboutUsPage'));
 const ServicesPage = lazy(() => import('./pages/ServicesPage'));
 const SingleServicePage = lazy(() => import('./pages/SingleServicePage'));
 const BlogPage = lazy(() => import('./pages/BlogPage'));
-const BlogSingle = lazy(() => import('./pages/BlogSingle'));
+const BlogSinglePage = lazy(() => import('./pages/BlogSinglePage'));
 const ContactPage = lazy(() => import('./pages/ContactPage'));
 // Component imports
 const Header = lazy(() => import('./components/organisms/Header'));
@@ -29,7 +29,7 @@ function App() {
             <Route path='/services' element={<ServicesPage />} />
             <Route path='/services/service' element={<SingleServicePage />} />
             <Route path='/blog' element={<BlogPage />} />
-            <Route path='/blog/single' element={<BlogSingle />} />
+            <Route path='/blog/single' element={<BlogSinglePage />} />
             <Route path='/contact' element={<ContactPage />} />
           </Routes>
         </Suspense>
