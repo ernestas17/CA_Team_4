fddbd62aa4d94b436a5e5e77890cbf18954182f0
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-09-19 12:04:49 +0300

    Link added

--- src/pages/BlogPage/components/ArticlesSection/Article/Article.jsx
+++ src/pages/BlogPage/components/ArticlesSection/Article/Article.jsx
@@ -1,11 +1,15 @@
 import { StyledArticle } from './Article.style';
+import { Link } from 'react-router-dom';
 
 const Article = ({ img, date, title, content }) => {
   return (
     <StyledArticle img={img}>
       <div></div>
       <p>{date}</p>
-      <h5>{title}</h5>
+      <Link to={'/blog/single'}>
+        <h5>{title}</h5>
+      </Link>
+
       <p>{content}</p>
     </StyledArticle>
   );
