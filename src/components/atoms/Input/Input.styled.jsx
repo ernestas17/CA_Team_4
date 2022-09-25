3187d6b7d81d13efd14a775d658b0e2e63f5e4b7
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2022-09-25 17:34:15 +0300

    Prop for Input component added

--- src/components/atoms/Input/Input.styled.jsx
+++ src/components/atoms/Input/Input.styled.jsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const StyledInput = styled.input`
   width: 100%;
   padding: 22.91px 40.33px;
+  background-color: ${(props) => (props.primary ? '#fff' : '#F2F2F2')};
   border: none;
   outline: none;
 
   & ::placeholder {
     ${({ theme }) => theme.body2};
-    color: ${({ theme }) => theme.color.black};
-    opacity: 0.5;
+    color: ${(props) => (props.primary ? 'rgba(30, 27, 27, 0.5)' : '#1E1B1B')};
   }
 `;
